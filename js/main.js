ce03e1fc411845f2c7951e0f5951e50ba1ede58a
a26510 <fercho_1991_@hotmail.com>
2022-09-10 23:14:00 -0500

    otro

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ var encrypted_AES = CryptoJS.AES.encrypt("hola mundo", "Passphrase");
 console.log(encrypted_AES.toString())
 var decrypted_AES = CryptoJS.AES.decrypt(encrypted_AES, "Passphrase")
 console.log(decrypted_AES.toString(CryptoJS.enc.Utf8))
+
+function main(){
+    let acciones = document.querySelectorAll(".accion")
+    acciones.forEach((acccion)=>{
+        acccion.addEventListener("click", (evento)=>{
+            if (evento.target.id == "cifrar"){
+                console.log("cifrar")
+            }
+            if (evento.target.id == "descifrar"){
+                console.log("descifrar")
+            }
+            
+        })
+    })
+}
+
+main()
