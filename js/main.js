139e212cbc7709df0d77c5ffdd5ba57b0f2d7f16
a26510 <fercho_1991_@hotmail.com>
2022-09-12 18:52:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 async function main() {
-    const html = await import("./html.js")
-    const cifrado = await import("./cifrado.js")
-    const descifrado = await import("./descifrado.js")
-    // create the home section   
+    const html = await import("./html.js") 
     html.home()
     let acciones = document.querySelectorAll(".accion")
     acciones.forEach((acccion) => {
@@ -10,7 +7,8 @@ async function main() {
             if (evento.target.id == "cifrar") {
                 html.zona_cifrado()
                 let btn_cifrar = document.querySelector("#btn_cifrar")
-                btn_cifrar.addEventListener("click", _ => {
+                btn_cifrar.addEventListener("click", async _ => {
+                    const cifrado = await import("./cifrado.js")
                     let text_to_encrypt = document.querySelector("#text_to_encrypt")
                     let clave_cifrado = document.querySelector("#clave_cifrado")
                     if (text_to_encrypt.value == "" || clave_cifrado.value == "") {
@@ -18,14 +16,16 @@ async function main() {
                     }
                     if (text_to_encrypt.value != "" && clave_cifrado.value != "") {
                         let texto_cifrado = cifrado.texto_cifrado(text_to_encrypt.value, clave_cifrado.value)
-                        console.log(texto_cifrado)
+                        text_to_encrypt.value = texto_cifrado
+                        btn_cifrar.disabled = true
                     }
                 })
             }
             if (evento.target.id == "descifrar") {
                 html.zona_descifrado()
                 let btn_descifrar = document.querySelector("#btn_descifrar")
-                btn_descifrar.addEventListener("click", _ => {
+                btn_descifrar.addEventListener("click", async _ => {
+                    const descifrado = await import("./descifrado.js")
                     let text_to_decrypt = document.querySelector("#text_to_decrypt")
                     let clave_descifrado = document.querySelector("#clave_descifrado")
                     if (text_to_decrypt.value == "" || clave_descifrado.value == "") {
@@ -33,7 +33,9 @@ async function main() {
                     }
                     if (text_to_decrypt.value != "" && clave_descifrado.value != "") {
                         let texto_descifrado = descifrado.texto_descifrado(text_to_decrypt.value, clave_descifrado.value)
-                        console.log(texto_descifrado)
+                        text_to_decrypt.value = texto_descifrado
+                        btn_descifrar.disabled = true
+
                     }
                 })
             }
