9b27fefde40c006e78ad2141abfbdc0c0cbd696f
a26510 <fercho_1991_@hotmail.com>
2022-09-11 22:00:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// cifrado AES
-// var encrypted_AES = CryptoJS.AES.encrypt("hola mundo", "Passphrase");
-// console.log(encrypted_AES.toString())
-// var decrypted_AES = CryptoJS.AES.decrypt(encrypted_AES, "Passphrase")
-// console.log(decrypted_AES.toString(CryptoJS.enc.Utf8))
-
 async function main() {
     const html = await import("./html.js")
     const cifrado = await import("./cifrado.js")
